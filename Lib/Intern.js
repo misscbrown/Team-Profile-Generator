f99440bc6950f05fddf2b940cdeaa1adcebdaadc
cee breezy <djmisscbrown@hotmail.com>
2022-03-03 12:24:15 +0000

    Added Intern class to js

---
+++ Lib/Intern.js
@@ -0,0 +1,20 @@
+const Employee = require("./Employee");
+
+
+class Intern extends Employee {
+    constructor (name, id, email, github) {
+        super (name, id, email);
+        this.github = github;
+    }
+
+    getGithub() {
+        return this.github;
+    }
+
+    getRole() {
+        return "Intern";
+    }
+
+}
+
+module.exports = Intern;
