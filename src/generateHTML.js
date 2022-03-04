7730774b83a216ea5a8a234fee7bb517790c7846
cee breezy <djmisscbrown@hotmail.com>
2022-03-04 16:00:25 +0000

    generated new html file

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -66,10 +66,43 @@ const generateTeamMembersHtml = (teamMembers) => {
     
   };
 
+  generateHTML = (data) => {
+
+    // array for cards 
+    pageArray = []; 
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole(); 
+
+
+        // call manager function
+        if (role === 'Manager') {
+            const managerCard = generateManager(employee);
+
+            pageArray.push(managerCard);
+        }
+
+        // call engineer function
+        if (role === 'Engineer') {
+            const engineerCard = generateEngineer(employee);
+
+            pageArray.push(engineerCard);
+        }
+
+        // call intern function 
+        if (role === 'Intern') {
+            const internCard = generateIntern(employee);
+
+            pageArray.push(internCard);
+        }
+        
+    }
+
 
   // loop over the array of team members
 
-  
+
 
   // generateHTML = (data) => {
 
