d52767b2b2d4bf88b2718e1972004653864b1b50
cee breezy <djmisscbrown@hotmail.com>
2022-03-04 12:31:18 +0000

    Added question prompts for engineer

--- index.js
+++ index.js
@@ -57,6 +57,42 @@ function init() {
   }
 
   function addEngineer() {
+
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is the name of this engineer?",
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is the ID of this engineer?",
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the engineers email address?",
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is the Github address of this engineer?",
+      },
+    ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.engineerName,
+        answers.engineerId,
+        answers.engineerEmail,
+        answers.engineerGithub
+      );
+      console.log(engineer);
+      teamMembers.push(engineer);
+      chooseNextTeamMember();
+    });
+
     console.log("Adding Engineer....");
   }
 
