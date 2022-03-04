f24b3bd43f534624a3693d54fe23fa8be293da9d
cee breezy <djmisscbrown@hotmail.com>
2022-03-04 13:16:16 +0000

    Added html for generating manager and engineer

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -6,9 +6,21 @@ const generateTeamMembersHtml = (teamMembers) => {
   //function for generating html if manager
   const generateManager = (manager) => {
     //a string literal returned with the manager data
-
-    const managerHTML = ` 
     
+    const managerHTML = `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${manager.name}</h3>
+                <h4>Manager</h4><i class="material-icons">content_paste</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${manager.id}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="office">Office Number: ${manager.officeNumber}</p>
+            </div>
+        </div>
+    </div> 
     `
     // append your new string to the teamString on line 4
 
@@ -17,10 +29,28 @@ const generateTeamMembersHtml = (teamMembers) => {
   //function for generating html if engineer
   const generateEngineer = (engineer) => {
     //a string literal returned with the engineer data
+    `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+        </div>
+    </div>
+    `
   };
 
   //function for generating html if intern
   const generateIntern = (intern) => {
+    `
+    
+    `
     //a string literal returned with the intern data
   };
 
